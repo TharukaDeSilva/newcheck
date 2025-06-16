6a1f90594df27e95886b3e0f5a7ed4ba474fb1f9
Tharuka De Silva <71247681+Tharukads00@users.noreply.github.com>
2025-06-16 14:29:16 +0530

    test few methods

---
+++ array method1.js
@@ -0,0 +1,17 @@
+let fruits  = ["apple", "banana", "cherry", "woodapple", "orange", "pineapple", "grape", "kiwi", "mango", "papaya"];
+var vehicles = [];
+
+// joining arrays as string
+
+let a  = fruits.join(" ");
+
+let b =  a.split(" ")
+console.log(a);
+console.log(b);
+vehicles.push("car", "bike", "bus", "truck", "van");
+// print vehicles array
+console.log(vehicles);
+
+//delete vehicle array
+delete vehicles[2]
+console.log(vehicles)
